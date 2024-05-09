93d9fb179cb3989d45af940eeac916b0200d83fa
Aditya Hari_20210140044 <aldifaadit75@gmail.com>
2024-05-09 18:23:32 +0700

    Imported homeview.dart and updated MaterialApp properties in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:exe2/home/homeview.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
     );
   }
 }
